a0075741ee37c9311a37ea71c440b2423e334a22
Vincent Hwang <vincenthwang421@gmail.com>
2020-07-07 19:52:37 -0700

    started c data structures

---
+++ Data Structures/C/LinkedList.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct Node{
+     int item;
+     struct Node* next;
+};
+
+void add(struct Node* head, int item)
+{
+     struct Node* n = (struct Node*) malloc(sizeof(struct Node));
+
+     while()
+}
