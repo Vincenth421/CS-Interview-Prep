9125b98d4cc3eca073cbcec5043da8be41f7f953
Vincent Hwang <vincenthwang421@gmail.com>
2020-08-01 16:18:56 -0700

    added reverse linked list iterative

--- Algorithms/JavaAlgs.java
+++ Algorithms/JavaAlgs.java
@@ -169,7 +169,8 @@ class JavaAlgs{
          }
     }
 
-    public ListNode reverseList(ListNode head) {
+    //recursive
+    public ListNode reverseListRec(ListNode head) {
         if(head == null || head.next == null) {
             return head;
         }
@@ -183,6 +184,28 @@ class JavaAlgs{
         // send back new head node in every recursion
         return newHeadNode;
     }
+
+    //iterative
+    public ListNode reverseListIter(ListNode head) {
+        if(head == null) return null;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while(curr.next != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        curr.next = prev;
+
+        return curr;
+    }
+
 
     /** Dynamic Programming **/
 
